3f858196c18b9f87d02583e27d917481471c35e8
Ewan Chou <z@pingcap.com>
2017-06-06 07:27:30 +0800

    variable: add a varaible to read current timestamp (#3400)

--- sessionctx/variable/tidb_vars.go
+++ sessionctx/variable/tidb_vars.go
@@ -51,6 +51,10 @@ const (
 	// those indices can be scanned concurrently, with the cost of higher system performance impact.
 	TiDBBuildStatsConcurrency = "tidb_build_stats_concurrency"
 
+	// TiDBCurrentTS is used to get the current transaction timestamp.
+	// It is read-only.
+	TiDBCurrentTS = "tidb_current_ts"
+
 	/* Session and global */
 
 	// tidb_distsql_scan_concurrency is used to set the concurrency of a distsql scan task.
@@ -109,4 +113,5 @@ const (
 	DefOptAggPushDown             = true
 	DefOptInSubqUnfolding         = false
 	DefBatchInsert                = false
+	DefCurretTS                   = 0
 )
